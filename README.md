8fb3ce92eaf4cc9c0dfc640eaa2781bc0026222e
keemeesuu <kimhogy@gmail.com>
2023-04-28 14:21:05 +0900

    simple calendar

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
-# lab
 
-A new Flutter project.
+## Future Builder 다루기
 
-## Getting Started
+비동기적 데이터 작업이 필요할때 사용한다.
 
-This project is a starting point for a Flutter application.
+### 가능 예
+비동기적 데이터를 받아. 특정 위젯을 rebuild 시키며 받은 데이터를 위젯에 전달 할 수 있다.
 
-A few resources to get you started if this is your first Flutter project:
+비동기 처리가 안되는 위젯을 처리할때 유용하다. 본인은 TableCalendar와 Firebase의 데이터를 Future로 받지 못해서 해결책으로 FutureBuilder를 생각했다.
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+FutureBuild의 future를 다시 사용하기 위해 setState를 써야한다는 특징이 있다.
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+TableCalendar 이벤트 변경중일때 setState가 안되서 FutureBuilder는 맞지 않는다.
+
+[FutureBuilder와 AsyncSnapshot](http://www.incodom.kr/Flutter/FutureBuilder#h_3c6f298a67ad3c4c005a9fc6e42a3cf1)
+
+
+[[Flutter]플러터 Futurebuilder / Streambuilder란??](https://devmg.tistory.com/183)
+
+
+## StreamBuilder
+꼭 서버통신만 할 수 있는게 아니다.
+변수의 변화도 구독할 수 있다.
+
+
+## Simple Calendar
+
+### 사용한 패키지
+
+- intl : flutter의 공식 다국어 지원 패키지. 언어 번역이나 단수/복수, 성별, 날짜/숫자를 그 지역이나 국가에 맞게 바꿀 수 있
--- lib/features/simple_calendar/simple_calendar.dart
+++ lib/features/simple_calendar/simple_calendar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'package:lab/features/simple_calendar/date_time.dart';
 import 'package:lab/features/simple_calendar/widgets/dayOfWeek.dart';
 
@@ -14,83 +16,128 @@ class _SimpleCalendarState extends State<SimpleCalendar> {
 
   List<DateTime> date = [];
 
-  List<String> dayNameOfWeek = ['월', '화', '수', '목', '금', '토', '일'];
+  /// Start weeko on
+  /// 1: Mon, 2: Tue, 3: Wed, 4: Thu, 5: Fri, 6: Sat, 7: Sun
+  final int _startWeekAt = 1;
+
+  /// Names of Days
+  /// Mon, Thu, Wed, Thu, Fri, Sat, Sun
+  late List<String> _weekNames;
+
+  /// Todays date
+  final DateTime _today = DateTime.utc(2023, 05, 01);
+
+  /// The Calendar displays the day of the week
   List<DateTime> _daysOfWeek = [];
 
-  final DateTime _today = DateTime.utc(2023, 04, 26);
+  /// Started day of the your week
+  late DateTime _statredDay;
 
-  /// 캘린더에서 현재 노출되는 날짜
-  DateTime _showsDay = DateTime.now();
+  /// Displays the current year
   String _showsYearName = "";
+
+  /// Displays the current month
   String _showsMonthName = "";
 
   @override
   void initState() {
     super.initState();
 
-    // 현재주 월~일 구하기
-    _daysOfWeek = _getWeeks(_today);
-    _showsYearName = _showsDay.year.toString();
-    _showsMonthName = _showsDay.month.toString();
+    // initializeDateFormatting('ko_KR', null);
+    // print(DateFormat.EEEE('ko_KR').format(DateTime.now()));
+
+    // Get names of the week
+    _weekNames = _getWeekName(_startWeekAt);
+
+    // Get fistday of your week
+    _statredDay = _getFirstDayOfWeek(_today);
+
+    // Displays the Current Day info
+    _daysOfWeek = _getWeeks(_statredDay);
+    _showsYearName = _statredDay.year.toString();
+    _showsMonthName = _statredDay.month.toString();
   }
 
-  /// 해당주의 월요일 구하기
-  DateTime _getMondayOfWeek(DateTime day) {
-    DateTime mondayOfWeek = day.subtract(Duration(days: day.weekday - 1));
-    return mondayOfWeek;
+  /// Get names of the week
+  List<String> _getWeekName(int day) {
+    int startAt = day - 1;
+    // default sunday
+    List<String> weekNames = ['월', '화', '수', '목', '금', '토', '일'];
+    //FIXME: 지역에 따라 다르게 설정하기
+
+    List<String> newWeekNames = [];
+
+    for (int i = startAt; i < weekNames.length; i++) {
+      newWeekNames.add(weekNames[i]);
+    }
+    for (int i = 0; i < startAt; i++) {
+      newWeekNames.add(weekNames[i]);
+    }
+
+    return newWeekNames;
   }
 
-  /// 지정날자의 주(월~일) 구하기
+  /// Get fist day of your week
+  DateTime _getFirstDayOfWeek(DateTime day) {
+    DateTime firstDayOfWeek =
+        day.subtract(Duration(days: day.weekday - _startWeekAt));
+
+    if (_startWeekAt > day.weekday) {
+      // 예) 현재 목요일인데 시작지정일이 금요일부터면 이전주 금요일 부터 가져와야 한다.
+      // 공식 : 이전주 + (시작요일 - 현재요일)
+
+      DateTime dayOfBeforeWeek = day.subtract(const Duration(days: 7));
+      firstDayOfWeek =
+          dayOfBeforeWeek.add(Duration(days: _startWeekAt - day.weekday));
+    }
+    return firstDayOfWeek;
+  }
+
+  /// Get days of your week
   List<DateTime> _getWeeks(DateTime day) {
-    int daysOfWeek = 7;
     List<DateTime> week = [];
-    DateTime monday = day.subtract(Duration(days: day.weekday - 1));
-    for (var i = 0; i < daysOfWeek; i++) {
-      week.add(monday.add(Duration(days: i)));
+
+    for (var i = 0; i < 7; i++) {
+      week.add(day.add(Duration(days: i)));
     }
 
     return week;
   }
 
-  /// 현재 캘린더에 보여지는 주의 다음주(월~일) 구하기
+  /// Get days next week
   void _getNextWeeks() {
-    DateTime mondayOfWeek =
-        _showsDay.subtract(Duration(days: _showsDay.weekday - 1));
-    DateTime mondayOfNextWeek = mondayOfWeek.add(const Duration(days: 7));
-    _showsDay = mondayOfNextWeek;
+    DateTime firstDayOfNextWeek = _statredDay.add(const Duration(days: 7));
+    _statredDay = firstDayOfNextWeek;
 
     int weekCount = 7;
     List<DateTime> week = [];
-    DateTime monday =
-        mondayOfNextWeek.subtract(Duration(days: mondayOfNextWeek.weekday - 1));
     for (var i = 0; i < weekCount; i++) {
-      week.add(monday.add(Duration(days: i)));
+      week.add(firstDayOfNextWeek.add(Duration(days: i)));
     }
 
     setState(() {
-      _showsYearName = _showsDay.year.toString();
-      _showsMonthName = _showsDay.month.toString();
+      _showsYearName = _statredDay.year.toString();
+      _showsMonthName = _statredDay.month.toString();
       _daysOfWeek = week;
     });
   }
 
-  /// 현재 캘린더에 보여지는 주의 이전(월~일) 구하기
+  /// Get days previous week
   void _getPrevWeeks() {
-    DateTime mondayOfPrevWeek =
-        _showsDay.subtract(Duration(days: _showsDay.weekday));
-    _showsDay = mondayOfPrevWeek;
+    DateTime firstDayOfPreviousWeek =
+        _statredDay.subtract(const Duration(days: 7));
+
+    _statredDay = firstDayOfPreviousWeek;
 
     int weekCount = 7;
     List<DateTime> week = [];
-    DateTime monday =
-        mondayOfPrevWeek.subtract(Duration(days: mondayOfPrevWeek.weekday - 1));
     for (var i = 0; i < weekCount; i++) {
-      week.add(monday.add(Duration(days: i)));
+      week.add(firstDayOfPreviousWeek.add(Duration(days: i)));
     }
 
     setState(() {
-      _showsYearName = _showsDay.year.toString();
-      _showsMonthName = _showsDay.month.toString();
+      _showsYearName = _statredDay.year.toString();
+      _showsMonthName = _statredDay.month.toString();
       _daysOfWeek = week;
     });
   }
@@ -154,7 +201,7 @@ class _SimpleCalendarState extends State<SimpleCalendar> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                for (var day in dayNameOfWeek)
+                for (var day in _weekNames)
                   DayOfWeek(
                     name: day,
                   ),
